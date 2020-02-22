c03e389bd4fde591360dbe9365d886ac7e084d12
lumen1296 <lumen1996@gmail.com>
2020-02-21 23:30:42 -0500

    feat: server.js config

---
+++ server.js
@@ -0,0 +1,17 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/invoice-api-front'));
+
+app.get('/*', function(req,res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.sendFile(path.join(__dirname+'/dist/invoice-api-front/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
