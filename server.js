0b1f3456f1ae850be55e27614fdfa34fb21f814c
lumen1296 <lumen1996@gmail.com>
2020-02-22 13:35:17 -0500

    Add config

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/invoice-api-front'));
 
-app.get('/*', function(req,res) {
+app.get('/*', (req,res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.sendFile(path.join(__dirname+'/dist/invoice-api-front/index.html'));
+  
 });
 
 // Start the app by listening on the default Heroku port
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { HomeComponent } from './modules/home/home.component';
 
 const routes: Routes = [
   {
-    path: '/',
-    component: HomeComponent
-  }
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
